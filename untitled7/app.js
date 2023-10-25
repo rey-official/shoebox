aa90a9281307f7e5181b377156981b77c1aaaae8
Mathenge001 <132335713+rey-official@users.noreply.github.com>
2023-10-25 11:43:04 +0300

    Add files via upload

---
+++ untitled7/app.js
@@ -0,0 +1,174 @@
+const wrapper = document.querySelector(".slideWrapper")
+const menuItems = document.querrySelectorAll(".menuItem")
+
+const products =[
+  {
+    id: 1,
+    title: "Fluffy sandals",
+    price: 2000,
+    colors: [
+      {
+        code: "black",
+        img: "./img/fluffy white background.jpg",
+      },
+      {
+        code: "darkblue",
+        img: "./img/fluffy sandals green.jpg",
+      },
+    ]
+  },
+  {
+    id: 2,
+    title: "Lilac sandals",
+    price: 1800,
+    colors:[
+      {
+        code: "black",
+        img: "./img/lilac sandals black.jpg",
+      },
+      {
+        code: "pink",
+        img: "./img/lilac sandals pink.jpg",
+      },
+      {
+        code: "white",
+        img: "./img/lilac sandals white.jpg",
+      },
+      {
+        code: "red",
+        img: "./img/lilac sandals red.jpg",
+      },
+      {
+        code: "yellow"
+        img: "./img/lilac sandals yellow.jpg",
+      },
+    ]
+  },
+  {
+    id: 3
+    title: "Jordan 1 low Travisscott New Season",
+    price: 3500,
+    colors: [
+      {
+        code: "blue",
+        img: "./img/jordan 1 low travis scott rear view.jpg",
+      },
+    ]
+  },
+  {
+    id: 4,
+    title: "Chain sandals",
+    price: 2100,
+    colors: [
+      {
+        code: "pink",
+        img: "./img/chain sandals pink.jpg",
+      },
+      {
+        code: "red",
+        img: "./img/chain sandals red.jpg",
+      },
+      {
+        code: 'white',
+        img: "./img/chain sandals white.jpg",
+      },
+    ]
+  },
+  {
+    id: 5,
+    title: "Adidas Samba New Season",
+    price: 3200,
+    colors: [
+      {
+        code: "black",
+        img: "./img/adidas samba black, white stripes.jpg",
+      },
+      {
+        code: "white",
+        img: "./img/adidas samba white,black stripes.jpg",
+      },
+      {
+        code: "white",
+        img: "./img/adidas samba whte.jpg",
+      },
+    ]
+  },
+    {
+       id: 6,
+    title: "Airmax TM New Season",
+    price: 3300,
+    colors:[
+      {
+        code: "gold",
+        img: "./img/airmax. TM golden.jpg",
+      },
+      {
+        code: "green",
+        img: "./img/airmax.TM green.jpg",
+      },
+      {
+        code: "black",
+        img: "./img/airmax TM black.jpg"
+      },
+    ],
+  },
+];
+
+let choosenProduct = product[0]
+
+const currentProductImg = document.querySelector(".productImg");
+const currentProductTitle = document.querySelector(".productTitle");
+const currentProductPrice = document.querySelector(".productPrice");
+const currentProductColors = document.querySelectorAll(".color");
+const currentProductSizes = document.querySelectorAll(".size");
+
+menuItems.forEach(item,index)=>{
+  item.addEventListener("click, ()=>"){
+    //chage the current slide
+    wrapper.style.transform= `translateX(${-100 * index}vw)`;
+
+    //change the choosen product
+    choosenProduct = products[index]
+
+    //change texts of currentProduct
+    currentProductTitle.textContent = choosenProduct.title
+    currentProductPrice.textContent = "ksh" + choosenProduct.price
+    currentProductImg.src = choosenProduct.colors[0].img
+
+    //passing new colors
+    currentProductColors.forEach((color, index) => {
+      color.style.backgroundColor = choosenProduct.colors[index].code;
+    });
+  });
+});
+
+
+currentProductColors.forEach((color, index)=>{
+  color.addEventListener("click", () =>{
+    currentProductImg.src = choosenProduct.colors[index].img;
+  });
+});
+
+currentProductSizes.forEach((size, index)=>{
+  size.addEventListener("click",()=>{
+    currentProductSizes.forEach((size)=>{
+      size.style.backgroundColor = "white";
+      size.style.color = "black";
+    });
+    size.style.backgroundColor= "black"
+    size.style.color= "white"
+  });
+});
+
+
+const productButton = document.querrySeletor(".productButton");
+const payment = document.querrySeletor(".payment");
+const close = document.querrySeletor(".close");
+
+productButton.addEventListener("click",()=>{
+  payment.style.display="flex"
+});
+
+  close.addEventListener("click",()=>{
+  payment.style.display="none";
+});
